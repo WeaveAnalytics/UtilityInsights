4c03a6f1f0eb02ce76b349424904a591713a2c27
Tonio Lora <tonio.lora@outlook.com>
2025-04-18 10:48:25 -0500

    Assign current user as admin too

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from needlr import auth, FabricClient
 from needlr.auth import FabricServicePrincipal
+from needlr.models.workspace import Workspace, WorkspaceRole, ServicePrincipal
+
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +12,8 @@ load_dotenv()
 api_id = os.getenv("APP_ID")
 api_key = os.getenv("APP_SECRET")
 tenant = os.getenv("TENANT_ID")
+capacity = os.getenv("CAPACITY_NAME")
+current_user_id = os.getenv("CURRENT_USER_ID")
 
 # Print them for demonstration (avoid this in production)
 print(f"API Id: {api_id}")
@@ -21,15 +25,18 @@ auth = FabricServicePrincipal(api_id, api_key, tenant)
 fc = FabricClient(auth=auth)
 # Get Capacity
 for c in fc.capacity.list_capacities():
-    if c.displayName.startswith("Trial"):
+    if c.displayName == capaciy:
         c_id = str(c.id)
         break
 # Create Workspace
-ws = fc.workspace.create(display_name='TestWS',
+ws = fc.workspace.create(display_name='UtilityInsightsWS',
                              capacity_id= c_id, 
                              description='UtilityInsights WS')
 # Check WS creation
 for ws in fc.workspace.ls():
     print(f"{ws.name}: Id:{ws.id} Capacity:{ws.capacityId}")
-        
+
+# Assign current user as Admin of newly created WS
+fc.workspace.role.assign(workspace_id=workspace_test.id, principal=ServicePrincipal(id=current_user_id), role=WorkspaceRole.Admin)
+
 print("Done")
