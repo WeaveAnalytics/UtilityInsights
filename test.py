2e1fe04315de437a97fc75a58e3adcc57a4a7af9
Tonio Lora <tonio.lora@outlook.com>
2025-04-18 11:15:49 -0500

    Tracking Capacity setting change

--- test.py
+++ test.py
@@ -18,16 +18,21 @@ current_user_id = os.getenv("CURRENT_USER_ID")
 # Print them for demonstration (avoid this in production)
 print(f"API Id: {api_id}")
 print(f"Database Host: {api_key}")
-print(f"Database Port: {tenant}")
+print(f"Tenant: {tenant}")
+print(f"Capacity: {capacity}")
+print(f"Current User: {current_user_id}")
 
 # Connect
 auth = FabricServicePrincipal(api_id, api_key, tenant)
 fc = FabricClient(auth=auth)
 # Get Capacity
+c_id = ""
 for c in fc.capacity.list_capacities():
-    if c.displayName == capaciy:
+    print(c.displayName)
+    if c.displayName == capacity:
         c_id = str(c.id)
         break
+print(c_id)
 # Create Workspace
 ws = fc.workspace.create(display_name='UtilityInsightsWS',
                              capacity_id= c_id, 
@@ -37,6 +42,7 @@ for ws in fc.workspace.ls():
     print(f"{ws.name}: Id:{ws.id} Capacity:{ws.capacityId}")
 
 # Assign current user as Admin of newly created WS
-fc.workspace.role.assign(workspace_id=workspace_test.id, principal=ServicePrincipal(id=current_user_id), role=WorkspaceRole.Admin)
+fc.workspace.role.assign(workspace_id=ws.id, principal=ServicePrincipal(id=current_user_id), role=WorkspaceRole.Admin)
 
 print("Done")
+
