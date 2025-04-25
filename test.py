43abeb5b57093ecd8ef2b1b0068d6d5bc5777b81
Tonio Lora <tonio.lora@outlook.com>
2025-04-25 13:57:17 -0500

    Fixed Workspace creation to use new Workspace name parameter

--- test.py
+++ test.py
@@ -13,14 +13,8 @@ api_id = os.getenv("APP_ID")
 api_key = os.getenv("APP_SECRET")
 tenant = os.getenv("TENANT_ID")
 capacity = os.getenv("CAPACITY_NAME")
+workspace = os.getenv("FABRIC_WORKSPACE")
 current_user_id = os.getenv("CURRENT_USER_ID")
-
-# Print them for demonstration (avoid this in production)
-print(f"API Id: {api_id}")
-print(f"Database Host: {api_key}")
-print(f"Tenant: {tenant}")
-print(f"Capacity: {capacity}")
-print(f"Current User: {current_user_id}")
 
 # Connect
 auth = FabricServicePrincipal(api_id, api_key, tenant)
@@ -34,15 +28,11 @@ for c in fc.capacity.list_capacities():
         break
 print(c_id)
 # Create Workspace
-ws = fc.workspace.create(display_name='UtilityInsightsWS',
-                             capacity_id= c_id, 
-                             description='UtilityInsights WS')
-# Check WS creation
-for ws in fc.workspace.ls():
-    print(f"{ws.name}: Id:{ws.id} Capacity:{ws.capacityId}")
+ws = fc.workspace.create(display_name=workspace,
+                             capacity_id=c_id, 
+                             description='Utility Insights')
 
 # Assign current user as Admin of newly created WS
 fc.workspace.role.assign(workspace_id=ws.id, principal=ServicePrincipal(id=current_user_id), role=WorkspaceRole.Admin)
 
 print("Done")
-
