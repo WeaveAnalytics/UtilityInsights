789f027aa47c15c3a80b31b1ef94fb192fe9d9f0
Tonio Lora <tonio.lora@outlook.com>
2025-04-25 10:08:58 -0500

    Tabs fixed on all if statements

--- azure_deploy.sh
+++ azure_deploy.sh
@@ -14,11 +14,10 @@ app_name="${APP_NAME}${suffix}"
 fabric_capacity_name="${FABRIC_CAPACITY_NAME}${suffix}"
 fabric_capacity_sku=$FABRIC_CAPACITY_SKU
 
-
 # If Resource Gorup already exists, use it. Otherwise create a new one using suffix
 if [ $(az group exists --name $RESOURCE_GROUP) = false ]; then
     echo "Resource Group does not exist. Creating the resource group ${resource_group} ..."
-    az group create --name $resource_group --location $location
+    az group create --name $resource_group --location $location
 else
     echo "Resource group ${RESOURCE_GROUP} already exists. It will be used on this deployment."
     resource_group=$RESOURCE_GROUP
@@ -28,19 +27,19 @@ fi
 SERVICE_EXISTS=$(az resource list --resource-type "Microsoft.CognitiveServices/accounts" --query "[?name=='$ACCOUNT_NAME']" --output tsv)
 if [ -z "$SERVICE_EXISTS" ]; then
     echo "Azure OpenAI account does not exist. Creating Azure OpenAI account ${account_name} ..."
-    az cognitiveservices account create --name $account_name --resource-group $resource_group --kind OpenAI --sku S0 --location $location
+    az cognitiveservices account create --name $account_name --resource-group $resource_group --kind OpenAI --sku S0 --location $location
 else
     echo "Azure OpenAI account ${ACCOUNT_NAME} already exists. It will be used on this deployment."
-    account_name=$ACCOUNT_NAME
+    account_name=$ACCOUNT_NAME
 fi
 
 # Check if the Azure OpenAI model deployment exists
 DEPLOYMENT_EXISTS=$(az openai deployment list --resource-group $resource_group --resource-name $account_name --query "[?name=='$DEPLOYMENT_NAME']" --output tsv)
 if [ -z "$DEPLOYMENT_EXISTS" ]; then
-    echo "Model deployment does not exist. Deploying model ${deployment_name} ..."
-    az cognitiveservices account deployment create --resource-group $resource_group --name $account_name --model-name gpt-4o --model-version 2024-11-20 --model-format OpenAI --sku GlobalStandard --deployment-name $deployment_name --sku-capacity 100
+    echo "Model deployment does not exist. Deploying model ${deployment_name} ..."
+    az cognitiveservices account deployment create --resource-group $resource_group --name $account_name --model-name gpt-4o --model-version 2024-11-20 --model-format OpenAI --sku GlobalStandard --deployment-name $deployment_name --sku-capacity 100
 else
-    echo "Model deployment already exists. It will be used on this deployment"
+    echo "Model deployment already exists. It will be used on this deployment"
     deployment_name=$DEPLOYMENT_NAME
 fi
 # Retrieve endpoint URL and API key
@@ -48,44 +47,41 @@ fi
 endpoint_url="https://${location}.api.cognitive.microsoft.com/openai/deployments/${deployment_name}/chat/completions?api-version=2025-01-01-preview"
 api_key=$(az cognitiveservices account keys list --name $account_name --resource-group $resource_group --query "key1" --output tsv)
 
-
 # Check if the Key Vault exists
 KEYVAULT_EXISTS=$(az keyvault list --query "[?name=='$KEYVAULT_NAME']" --output tsv)
 if [ -z "$KEYVAULT_EXISTS" ]; then
-    echo "Key Vault does not exist. Creating the Key Vault ${keyvault_name} ..."
-    current_user_name=$(az ad signed-in-user show --query userPrincipalName -o tsv)
+    echo "Key Vault does not exist. Creating the Key Vault ${keyvault_name} ..."
+    current_user_name=$(az ad signed-in-user show --query userPrincipalName -o tsv)
     current_user_id=$(az ad signed-in-user show --query id -o tsv)
     az keyvault create --name $keyvault_name --resource-group $resource_group --location $location
     az role assignment create --assignee ${current_user_id} --role "Key Vault Secrets Officer" --scope $(az keyvault show --name ${keyvault_name} --resource-group ${resource_group} --query id -o tsv)
     sleep 10
 else
-    echo "Key Vault already exists. It will be used on this deployment""
+    echo "Key Vault already exists. It will be used on this deployment""
     keyvault_name=$KEYVAULT_NAME
 fi
-
 
 # Check if gpt4ourl secret exists
 SECRET_EXISTS=$(az keyvault secret list --vault-name $keyvault_name --query "[?name=='gpt4ourl']" --output tsv)
 if [ -z "$SECRET_EXISTS" ]; then
-    echo "Secret does not exist. Creating secrets gpt4ourl ..."
-    az keyvault secret set --vault-name $keyvault_name --name gpt4ourl --value $endpoint_url
+    echo "Secret does not exist. Creating secrets gpt4ourl ..."
+    az keyvault secret set --vault-name $keyvault_name --name gpt4ourl --value $endpoint_url
     az keyvault secret set --vault-name $keyvault_name --name gpt4okey --value $api_key
 else
-    echo "Secrets already exists."
+    echo "Secrets already exists."
 fi
-
 
 APP_EXISTS=$(az ad app list --display-name $APP_NAME --query "[?displayName=='$APP_NAME']" --output tsv)
 if [ -z "$APP_EXISTS" ]; then
-    echo "App registration does not exist. Creating the app registration ${app_name}..."
-    az ad app create --display-name ${app_name}
+    echo "App registration does not exist. Creating the app registration ${app_name}..."
+    az ad app create --display-name ${app_name}
     app_id=$(az ad app list --query "[?displayName=='${app_name}'].appId" --output tsv)
     sleep 5
     az ad app permission add --id $app_id --api "00000009-0000-0000-c000-000000000000" --api-permissions 28379fa9-8596-4fd9-869e-cb60a93b5d84=Role
     sleep 5
     az ad app permission admin-consent --id $app_id
 else
-    echo "App registration already exists."
+    echo "App registration already exists."
     app_name=$APP_NAME
 fi
 
@@ -96,15 +92,14 @@ secret="${secret//\"}"
 tenant=$(echo $output | jq '.tenant')
 tenant="${tenant//\"}"
 
-
 # Check if the Microsoft Fabric capacity exists
 CAPACITY_EXISTS=$(az fabric capacity list --query "[?name=='$FABRIC_CAPACITY_NAME']" --output tsv)
 if [ -z "$CAPACITY_EXISTS" ]; then
-    echo "Fabric capacity does not exist. Creating the capacity ${fabric_capacity_name} with ${fabric_capacity_sku} SKU ..."
-    az fabric capacity create --resource-group $resource_group --capacity-name $fabric_capacity_name --sku "{name:${fabric_capacity_sku},tier:Fabric}" --location $location --administration "{members:[${current_user_name}]}"
+    echo "Fabric capacity does not exist. Creating the capacity ${fabric_capacity_name} with ${fabric_capacity_sku} SKU ..."
+    az fabric capacity create --resource-group $resource_group --capacity-name $fabric_capacity_name --sku "{name:${fabric_capacity_sku},tier:Fabric}" --location $location --administration "{members:[${current_user_name}]}"
     sleep 5
 else
-    echo "Fabric capacity already exists."
+    echo "Fabric capacity already exists."
     fabric_capacity_name=$FABRIC_CAPACITY_NAME
 fi
 
