e8fe6362cdd0a4accbdf5a99aaff124e6cbd44b6
Tonio Lora <tonio.lora@outlook.com>
2025-04-25 09:18:12 -0500

    Create parameters.sh

---
+++ parameters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Deployment Parameters (prefix)
+RESOURCE_GROUP="UtilityInsightsRG"
+LOCATION="eastus"
+ACCOUNT_NAME="utilityinsightsoai"
+DEPLOYMENT_NAME="utilityinsightsgpt4o"
+KEYVAULT_NAME="utilityinsightskv"
+FABRIC_CAPACITY_NAME="utilityinsightsfc"
+FABRIC_CAPACITY_SKU="F64"
+APP_NAME="utilityinsightsfapp"
