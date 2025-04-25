6325a23373e55ab198643fad2cac784eaba30fb0
Tonio Lora <tonio.lora@outlook.com>
2025-04-25 09:56:16 -0500

    Update parameters.sh

--- parameters.sh
+++ parameters.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Deployment Parameters (prefix)
+# If RG already exists it will be used. 
 RESOURCE_GROUP="UtilityInsightsRG"
 LOCATION="eastus"
+# If Azure OpenAI service already exists it will be used. 
 ACCOUNT_NAME="utilityinsightsoai"
+# If Azure OpenAI model is already deployed it will be used. 
 DEPLOYMENT_NAME="utilityinsightsgpt4o"
+# If Azure KeyVault is already deployed it will be used. 
 KEYVAULT_NAME="utilityinsightskv"
+# If Entra App Registration is already deployed it will be used. 
+APP_NAME="utilityinsightsfapp"
+# If Fabric Capacity is already deployed it will be used. 
 FABRIC_CAPACITY_NAME="utilityinsightsfc"
 FABRIC_CAPACITY_SKU="F64"
-APP_NAME="utilityinsightsfapp"
+
